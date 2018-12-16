cf08c9f147f69c6f37a6c2a7b127250225dfa0cf
Itsaraporn Chaichayanon <ItsaraChai@ItsaraChais-MacBook-Pro.local>
2018-12-16 09:52:17 +0700

    Create fastlane

--- TestJenkinsTests/TestJenkinsTests.swift
+++ TestJenkinsTests/TestJenkinsTests.swift
@@ -18,9 +18,9 @@ class TestJenkinsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testFirstTestJenkins() {
+        
+        XCTAssert(true)
     }
 
     func testPerformanceExample() {
