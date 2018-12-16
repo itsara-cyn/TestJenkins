d626170a8dd895c6c1b9579140b01453326de736
Itsaraporn Chaichayanon <ItsaraChai@ItsaraChais-MacBook-Pro.local>
2018-12-16 09:23:58 +0700

    Create project

---
+++ TestJenkins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by Itsaraporn Chaichayanon on 16/12/2561 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
